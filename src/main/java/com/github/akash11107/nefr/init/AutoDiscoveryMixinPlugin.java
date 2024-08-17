da837c3d19366b77d3ca7243be08430a19dbd37e
GitHub Action <action@github.com>
2024-08-17 06:27:35 +0000

    Initialize template repository proper
    Set package name to com.github.akash11107.nefr
    Set project name to NEFR
    Set modid to nefr

--- src/main/java/com/github/akash11107/nefr/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/akash11107/nefr/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.akash11107.nefr.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/akash11107/nefr/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/akash11107/nefr/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.akash11107.nefr.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
